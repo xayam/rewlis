26b62c1de396e2e797e2e1493ed6cf9375411428
xayam <xayam@yandex.ru>
2024-09-24 17:48:25 +0300

    v3.58

--- rewlis/controller/audio.py
+++ rewlis/controller/audio.py
@@ -8,19 +8,27 @@ class AudioClass:
     def __init__(self, cprint, audio_list, output, language):
         self.cprint = cprint
         self.audio_list = audio_list
+        self.output = output
         self.language = language
-        self.MP3 = f"{output}/{self.language}.mp3"
-        self.WAV = [f"{output}/wav{self.language}/{i[1][-4:]}.wav"
+        self.MP3 = f"{self.output}/{self.language}.mp3"
+        self.WAV = [f"{self.output}/wav{self.language}/{i[1][:-4]}.wav"
                     for i in self.audio_list]
-        self.FLAC = f"{output}/{self.language}.flac"
+        self.FLAC = f"{self.output}/{self.language}.flac"
         if self.audio_list:
-            t1 = threading.Thread(target=self.create_wav)
-            t1.start()
-            t2 = threading.Thread(target=self.create_mp3_flac)
-            t2.start()
+            t = threading.Thread(target=self.process)
+            t.start()
+            t.join()
         else:
             self.cprint("Error: mp3 files not found")
             raise Exception("Error: mp3 files not found")
+
+    def process(self):
+        t1 = threading.Thread(target=self.create_wav)
+        t1.start()
+        t2 = threading.Thread(target=self.create_mp3_flac)
+        t2.start()
+        t1.join()
+        t2.join()
 
     def create_mp3(self):
         if os.path.exists(self.MP3):
@@ -30,17 +38,21 @@ class AudioClass:
         cbn.convert(samplerate=16000, n_channels=1)
         list_input = [f"{i[0]}/mp3{self.language}/{i[1]}"
                       for i in self.audio_list]
+        # self.cprint(list_input)
         cbn.build(list_input, self.MP3, 'concatenate')
 
     def create_wav(self):
         self.cprint("Converting mp3 to wav...")
         cbn = sox.Transformer()
         cbn.convert(samplerate=16000, n_channels=1)
-        for i in range(self.audio_list):
+        for i in range(len(self.audio_list)):
             if os.path.exists(self.WAV[i]):
                 continue
             self.cprint(f"Create '{self.WAV[i]}'...")
-            cbn.build(self.MP3, self.WAV[i])
+            cbn.build(
+                f"{self.audio_list[i][0]}/mp3{self.language}/" +
+                f"{self.audio_list[i][1]}",
+                self.WAV[i])
 
     def create_flac(self):
         if os.path.exists(self.FLAC):
